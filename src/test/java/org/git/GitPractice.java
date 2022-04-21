69de9084d48da32562d9bc1d6242dd582e11178e
GanesanBalakrishnan <grio965@gmail.com>
2022-04-21 12:49:53 +0530

    git checking

---
+++ src/test/java/org/git/GitPractice.java
@@ -0,0 +1,15 @@
+package org.git;
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(" HEllo World GIT ");
+		System.out.println("1st git program");
+		
+		
+		
+		
+	}
+}
